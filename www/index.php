a8ead96312d85cd59c42023342d03a0b5541421c
LeonardoBizzoni <leo2002714@gmail.com>
2022-04-08 16:43:51 +0200

    Aggiunto installazione server con docker

--- www/index.php
+++ www/index.php
@@ -3,15 +3,14 @@
 
 <head>
     <meta charset="UTF-8" />
-     <meta http-equiv="refresh" content="2" />
     <title>Document</title>
 </head>
 
 <body>
 
      <?php
-     include "./php/arduinoHandler.php";
-     include "./php/userHandler.php";
+     include "./arduinoHandler.php";
+     include "./userHandler.php";
      ?>
 </body>
 
