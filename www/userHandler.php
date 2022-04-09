11eb2e30907a50051e8f538e3b9f16149a68325c
LeonardoBizzoni <leo2002714@gmail.com>
2022-04-09 19:42:54 +0200

    Fix grammatica + frontend utente

--- www/userHandler.php
+++ www/userHandler.php
@@ -1,11 +1,19 @@
 <?php
-if (is_null($_POST["data"])) {
-    $sql = new mysqli("localhost:3306", "root", "", "DatiSensore") or die("Connection error:" . $sql->error);
-    $result = $sql->query("SELECT * FROM `Valori`");
+if (is_null($_POST["datiSensore"])) {
+    $sql = new mysqli("172.17.0.2:3306", "root", "root", "DatiSensore") or die("Connection error:" . $sql->error);
+    $result = $sql->query("SELECT num_persone, timestamp_lettura FROM `Valori` having hour(timestamp_lettura)=".date("H")." and day(timestamp_lettura)=".date("d")." and month(timestamp_lettura)=".date("m")." and year(timestamp_lettura)=".date("Y"));
+
+    echo '<div class="alert alert-primary" role="alert">Nell\'ultima ora sono state rilevate '.$result->num_rows.' persone!</div>';
 
     while ($row = $result->fetch_assoc()) {
-        print_r($row);
-        echo "</br>";
+        echo "
+<div class=\"card\" style=\"width: 18rem; margin:10px; padding:10px\">
+  <img src=\"avatar.jpg\" class=\"card-img-top\">
+  <div class=\"card-body\">
+    <p class=\"card-text\">Avatar registrato {$row["timestamp_lettura"]}</p>
+  </div>
+</div>
+";
     }
 
     $sql->close();
